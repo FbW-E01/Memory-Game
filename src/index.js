9081e5b4e95df744e78d282730d90b5af750d665
andreglock <andreglock@gmail.com>
2021-09-10 13:02:32 +0200

    added core timer functionality

--- src/index.js
+++ src/index.js
@@ -14,7 +14,27 @@ import 'regenerator-runtime';
 
 // END OF: Click and flip functionality
 // Timer Logic
+let flippedPairs = 0;
 
+const root = document.getElementById('root');
+const timer = document.createElement("div");
+timer.setAttribute("id", "timer");
+root.appendChild(timer);
+let deciseconds = 0;
+let seconds = 0;
+let minutes = 0;
+setInterval(() => {
+    deciseconds++;
+    if (deciseconds === 10) {
+        seconds++;
+        deciseconds = 0;
+    } if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+    }
+    timer.innerText = `${minutes}:${(seconds >= 10) ? '' : 0}${seconds}${deciseconds}`
+
+}, 100)
 
 // END OF: Timer Logic
 // Score manager
